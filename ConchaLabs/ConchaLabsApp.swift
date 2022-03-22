58e4506f028e2acdcc3e30ac10907f9dce264731
Alex Diaz <alexelo.swift@gmail.com>
2022-03-22 19:51:33 -0400

    Initial Commit

---
+++ ConchaLabs/ConchaLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConchaLabsApp.swift
+//  ConchaLabs
+//
+//  Created by Alexis Diaz on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ConchaLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
